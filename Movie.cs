050cc87e72f1137fbac51483173f7c8f4a0f2e93
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-07 01:43:24 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            this.title = aTitle;
+            this.director = aDirector;
+            this.rating = aRating;
+        }
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "PG-13")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie2 = new Movie("Unreleased", "Evan", "PG");
+            Console.WriteLine(movie1.Rating);
+            Console.WriteLine(movie2.Rating);
+            Console.ReadLine(); 
+        }
+    }
+}
